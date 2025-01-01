87721a893701090f1ee602d5191c7b006ed2bed7
Hamza86 <macbookprom3max@MACBOOKs-MacBook-Pro.local>
2025-01-01 21:03:59 +0300

    css auth and home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,15 @@
 const About = () => {
   return (
+
+
     <p>
-      At [GA Mart], we're more than just an online marketplace – we’re a
-      community-driven platform dedicated to empowering small and local grocery
-      stores. Our mission is simple: to help local businesses thrive by
-      connecting them to a broader customer base through our website. We
-      understand the challenges small grocery stores face in today’s competitive
-      market. That’s why we’ve created a platform designed to spotlight their
-      unique products, providing them with the tools and support they need to
-      grow. From fresh produce to specialty items, we’re committed to showcasing
-      the best from local vendors, ensuring customers have access to quality
-      goods while supporting their communities. By partnering with us, small
-      grocery stores can reach new customers, streamline their online presence,
-      and focus on what they do best – providing exceptional products and
-      service. Together, we’re building a network that strengthens local
-      economies and fosters sustainable growth. Join us in supporting small
-      businesses and creating a more connected, vibrant food community.
+At GA Mart, we're more than just an online marketplace – we’re a community-driven platform dedicated to empowering small and local grocery stores. Our mission is simple: to help local businesses thrive by connecting them to a broader customer base through our website.
+
+We understand the challenges small grocery stores face in today’s competitive market. That’s why we’ve created a platform designed to spotlight their unique products, providing them with the tools and support they need to grow. From fresh produce to specialty items, we’re committed to showcasing the best from local vendors, ensuring customers have access to quality goods while supporting their communities.
+
+By partnering with us, small grocery stores can reach new customers, streamline their online presence, and focus on what they do best – providing exceptional products and service. Together, we’re building a network that strengthens local economies and fosters sustainable growth.
+Join us in supporting small businesses and creating a more connected, vibrant food community.
+
     </p>
   )
 }
